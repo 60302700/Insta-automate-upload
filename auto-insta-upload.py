63dd16addb48196fd67900e8908d08837b0caf70
60302700 <60302700@udst.edu.qa>
2024-09-19 12:35:52 +0300

    Update auto-insta-upload.py

--- auto-insta-upload.py
+++ auto-insta-upload.py
@@ -12,8 +12,7 @@ def get_session_dict_from_env(env_var):
             print(f"Error decoding session data: {e}")
     return {}
 
-def get_days():
-    global bot
+def get_days(bot):
     bot.user_clips(bot.user_id,1)
     media = bot.user_clips(bot.user_id,1)
     latest = media[0]
@@ -22,14 +21,10 @@ def get_days():
     day = [d for d in cap[:15] if d.isdigit()]
     return int(''.join(day))+1
 
-DAY = get_days()
-
-def video_upload(USERNAME, PASSWORD, PATH, CAPTION,SESSION):
-    Insta = Client()
-    Insta.login(USERNAME, PASSWORD)
+def video_upload(PATH, CAPTION,SESSION,bot):
     time.sleep(5)
-    print(f"Logging in as {Insta.user_id}")
-    Insta.clip_upload(PATH, CAPTION)
+    print(f"Logging in as {bot.user_id}")
+    bot.clip_upload(PATH, CAPTION)
     print(f"Video uploaded: {PATH}")
 
 def scheduled_upload():
@@ -37,19 +32,25 @@ def scheduled_upload():
     USERNAME = str(os.getenv('USERNAME1'))
     PASSWORD = str(os.getenv('PASSWORD1'))
     SESSION = get_session_dict_from_env(os.getenv('SESSION1'))
+    Insta = Client()
+    Insta.load_settings(SESSION)
+    Insta.login(USERNAME, PASSWORD)
+    DAY = get_days(Insta)
     PATH = "grandpa.mp4"
-    global DAY
     CAPTION = f'''DAY {DAY} \n #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta'''
-    video_upload(USERNAME, PASSWORD, PATH, CAPTION,SESSION)
+    video_upload(PATH, CAPTION,SESSION,Insta)
 
 def scheduled_upload_benson():
     time.sleep(10)
     USERNAME = str(os.getenv('USERNAME2'))
     PASSWORD = str(os.getenv('PASSWORD2'))
     SESSION = get_session_dict_from_env(os.getenv('SESSION2'))
+    Insta = Client()
+    Insta.load_settings(SESSION)
+    Insta.login(USERNAME, PASSWORD)
+    DAY = get_days(Insta)
     PATH = "video.mp4"
-    global DAY
-    CAPTION = f"DAY {DAY+1} \n #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta"
-    video_upload(USERNAME, PASSWORD, PATH, CAPTION,SESSION)
+    CAPTION = f"DAY {DAY} \n #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta"
+    video_upload(PATH, CAPTION,SESSION,Insta)
 scheduled_upload()
 scheduled_upload_benson()
