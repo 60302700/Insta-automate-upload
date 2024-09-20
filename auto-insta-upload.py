bc7d6a72b64c492986b415299a0c5bdfbc934c7e
60302700 <60302700@udst.edu.qa>
2024-09-20 22:11:05 +0300

    Update auto-insta-upload.py

--- auto-insta-upload.py
+++ auto-insta-upload.py
@@ -14,7 +14,7 @@ def get_session_dict_from_env(env_var):
     return {}
 def get_days(user):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    h = requests.get(f'https://www.instagram.com/{user},'headers=headers)
+    h = requests.get(f'https://www.instagram.com/{user}',headers=headers)
     soup = BeautifulSoup(h.text,'html.parser')
     posts = soup.find('meta',property="og:description")
     posts = posts.prettify()
