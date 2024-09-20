45341dbc6871599bb7d597c75b96e59eaeed7ced
60302700 <60302700@udst.edu.qa>
2024-09-20 16:32:21 +0300

    Update auto-insta-upload.py

--- auto-insta-upload.py
+++ auto-insta-upload.py
@@ -2,7 +2,8 @@ from instagrapi import Client
 import os
 import time
 import json
-
+import requests
+from bs4 import BeautifulSoup
 def get_session_dict_from_env(env_var):
     session_str = os.getenv(env_var)
     if session_str:
@@ -11,6 +12,20 @@ def get_session_dict_from_env(env_var):
         except json.JSONDecodeError as e:
             print(f"Error decoding session data: {e}")
     return {}
+def get_days(user):
+h = requests.get(f'https://www.instagram.com/{user}')
+
+soup = BeautifulSoup(h.text,'html.parser')
+
+posts = soup.find('meta',property="og:description")
+
+posts = posts.get('content')
+
+posts = posts[1:posts.find('Posts')]
+
+return int((posts.split()[-1]))
+
+
 
 def video_upload(PATH, CAPTION,bot):
     time.sleep(5)
@@ -27,8 +42,9 @@ def scheduled_upload():
     Insta = Client()
     Insta.login(USERNAME, PASSWORD)
     Insta.set_settings(SESSION)
+    DAY = get_days(USERNAME)
     PATH = "grandpa.mp4"
-    CAPTION = f'''DAY test \n #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta'''
+    CAPTION = f'''DAY {DAY} \n #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta'''
     video_upload(PATH, CAPTION,Insta)
 
 def scheduled_upload_benson():
@@ -39,7 +55,7 @@ def scheduled_upload_benson():
     Insta = Client()
     Insta.login(USERNAME, PASSWORD)
     Insta.set_settings(SESSION)
-    DAY = get_days(Insta)
+    DAY = get_days(USERNAME)
     PATH = "video.mp4"
     CAPTION = f"DAY {DAY} \n #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta"
     video_upload(PATH, CAPTION,Insta)
