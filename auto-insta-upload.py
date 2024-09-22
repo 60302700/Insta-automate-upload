4272dd499bd99aa7743bb0074a6ad120bcbe2ea5
60307390 <60307390@udst.edu.qa>
2024-09-22 19:40:54 +0300

    edit auto-insta-upload.py, add id_user, why_u_always_lying account

--- auto-insta-upload.py
+++ auto-insta-upload.py
@@ -3,7 +3,9 @@ import os
 import time
 import json
 import requests
+from id_user import USER_IDS
 from bs4 import BeautifulSoup
+
 def get_session_dict_from_env(env_var):
     session_str = os.getenv(env_var)
     if session_str:
@@ -38,38 +40,52 @@ def video_upload(PATH, CAPTION,bot,user,password):
     bot.video_upload(PATH, CAPTION)
     print(f"Video uploaded: {PATH}")
 
-def scheduled_upload():
-    time.sleep(10)
-    USERNAME = str(os.getenv('USERNAME1'))
-    PASSWORD = str(os.getenv('PASSWORD1'))
-    SESSION = get_session_dict_from_env(os.getenv('SESSION1'))
-    id = 67987126316
+def timed_login(USERNAME, PASSWORD, SESSION)
     Insta = Client()
     bot.delay[1,10]
     Insta.set_settings(SESSION)
     bot.delay[1,10]
     Insta.login(USERNAME, PASSWORD)
     bot.delay[1,10]
+ 
+    return Insta
+
+def scheduled_upload(ID, USERNAME, PASSWORD, SESSION):
+    time.sleep(10)
+    Insta = timed_login(USERNAME, PASSWORD, SESSION)
     DAY = get_days(Insta,id)
     PATH = "grandpa.mp4"
     CAPTION = f'''DAY {DAY} \n #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta'''
     video_upload(PATH, CAPTION,Insta,USERNAME,PASSWORD)
 
-def scheduled_upload_benson():
+def scheduled_upload_benson(ID, USERNAME, PASSWORD, SESSION):
     time.sleep(10)
-    USERNAME = str(os.getenv('USERNAME2'))
-    PASSWORD = str(os.getenv('PASSWORD2'))
-    SESSION = get_session_dict_from_env(os.getenv('SESSION2'))
-    id = 67538479126
-    Insta = Client()
-    bot.delay[1,10]
-    Insta.login(USERNAME, PASSWORD) #674789
-    bot.delay[1,10]
-    Insta.set_settings(SESSION)
-    bot.delay[1,10]
+    Insta = timed_login(USERNAME, PASSWORD, SESSION)
     DAY = get_days(Insta,id)
     PATH = "video.mp4"
     CAPTION = f"DAY {DAY} \n #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta"
     video_upload(PATH, CAPTION,Insta,USERNAME,PASSWORD)
-scheduled_upload()
-#scheduled_upload_benson()
+
+def scheduled_upload_sc(ID, USERNAME, PASSWORD, SESSION):
+    time.sleep(10)
+    Insta = timed_login(USERNAME, PASSWORD, SESSION)
+    DAY = get_days(Insta,id)
+    PATH = f"{USERNAME.replace('.','_')}.mp4"
+    CAPTION = f'''Day {DAY} #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta'''
+    if ID == USER_IDS.LYING.value: CAPTION += '#whyualwayslying #why'
+    video_upload(PATH, CAPTION,Insta,USERNAME,PASSWORD)
+
+
+if __name__ == "__main__":
+    NO_OF_ACCOUNTS = len(USER_IDS)
+
+    for i in range(1,NO_OF_ACCOUNTS+1):
+        USERNAME=str(os.getenv(f'USERNAME{i}'))
+        PASSWORD=str(os.getenv(f'PASSWORD{i}'))
+        SESSION=get_session_dict_from_env(os.getenv(f'SESSION{i}'))
+
+        if i == 1: scheduled_upload(USER_IDS.GRANDPA.value, USERNAME, PASSWORD, SESSION)
+        elif i == 2: scheduled_upload_benson(USER_IDS.BENSON.value, USERNAME, PASSWORD, SESSION)
+        # elif i == 3: scheduled_upload_sc(USER_IDS.LYING.value,USERNAME, PASSWORD, SESSION)
+        # TODO: ACCOUNT 3 USER, PASS, SESSIOn
+        # elif i == 4, i == 5, ... for future accounts
---
+++ id_user.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+class USER_IDS(IntEnum):
+    GRANDPA=67987126316
+    BENSON=67538479126
+    LYING=69469114299
+
