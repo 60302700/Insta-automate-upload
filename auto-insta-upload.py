c06e287a655bf623b9d830f57fcbdf79cadb074b
60302700 <60302700@udst.edu.qa>
2024-09-24 13:40:19 +0300

    Update auto-insta-upload.py

--- auto-insta-upload.py
+++ auto-insta-upload.py
@@ -23,7 +23,7 @@ def get_days(bot,id):
     
 def video_upload(PATH, CAPTION,bot,user,password):
     time.sleep(5)
-    print(f"Logging in as {user}")
+    print(f"Logging in as {user+' '}")
     bot.login(user,password)
     bot.video_upload(PATH, CAPTION)
     print(f"Video uploaded: {PATH}")
