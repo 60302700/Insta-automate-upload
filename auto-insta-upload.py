61b75764aaedd36ffe1dae79961db596471364f9
Gowshikrajan <159643026+60307390@users.noreply.github.com>
2024-09-24 10:17:59 +0300

    Update auto-insta-upload.py

--- auto-insta-upload.py
+++ auto-insta-upload.py
@@ -64,8 +64,8 @@ if __name__ == "__main__":
         SESSION=get_session_dict_from_env(os.getenv(f'SESSION{i}'))
 
         #if i == 1: scheduled_upload(USER_IDS.GRANDPA.value, USERNAME, PASSWORD, SESSION)
-        if i == 2: scheduled_upload_benson(USER_IDS.BENSON.value, USERNAME, PASSWORD, SESSION)
-        elif i == 3: scheduled_upload_sc(USER_IDS.LYING.value,USERNAME, PASSWORD, SESSION)
-        elif i == 4: scheduled_upload_aq(USER_IDS.BRO.value, USERNAME, PASSWORD, SESSION)
+        if i == 2: scheduled_upload(USER_IDS.BENSON.value, USERNAME, PASSWORD, SESSION)
+        elif i == 3: scheduled_upload(USER_IDS.LYING.value,USERNAME, PASSWORD, SESSION)
+        elif i == 4: scheduled_upload(USER_IDS.BRO.value, USERNAME, PASSWORD, SESSION)
         # TODO: ACCOUNT 3 USER, PASS, SESSIOn
         # elif i == 4, i == 5, ... for future accounts
