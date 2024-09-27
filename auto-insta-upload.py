357fe58d89873c00c2faa7c2125471b842de7861
60307390 <60307390@udst.edu.qa>
2024-09-27 20:59:50 +0300

    update main py, depreciate id_user, add id list

--- auto-insta-upload.py
+++ auto-insta-upload.py
@@ -3,7 +3,6 @@ import os
 import time
 import json
 import requests
-from id_user import USER_IDS
 from bs4 import BeautifulSoup
 
 def get_session_dict_from_env(env_var):
@@ -20,7 +19,7 @@ def get_days(bot,id):
     vid = media[0].dict()['caption_text']
     return int(vid.split()[1])+1
     
-def video_upload(PATH, CAPTION,bot,user,password):
+def video_upload(PATH,CAPTION,bot,user,password):
     time.sleep(5)
     print(f"Logging in as {user+' '}")
     bot.login(user,password)
@@ -40,8 +39,16 @@ def scheduled_upload(ID, USERNAME, PASSWORD, SESSION):
     DAY = get_days(Insta,str(ID))
     PATH = f"{USERNAME.replace('.','_')}.mp4"
     CAPTION = f'''Day {DAY} #meme #trending #trending #viral #instagram #explorepage #explore #instagood #love #reels #follow #trend #like #photography #india #fyp #instadaily #tiktok #foryou #trendingreels #trendingnow #style #memes #photooftheday #music #reelsinstagram #viralpost #model #insta'''
-    if ID == USER_IDS.LYING.value: CAPTION += '#whyualwayslying #why'
+    if ID == USER_IDS[2]: CAPTION += '#whyualwayslying #why'
     video_upload(PATH, CAPTION,Insta,USERNAME,PASSWORD)
+
+USER_IDS = [
+        67987126316,    # GRANDPA
+        67538479126,    # BENSON
+        69469114299,    # LYING
+        69475320768     # BRO
+    ]
+
 
 
 if __name__ == "__main__":
@@ -53,13 +60,13 @@ if __name__ == "__main__":
         SESSION=get_session_dict_from_env(os.getenv(f'SESSION{i}'))
 
         try: 
-            if i == 1: scheduled_upload(USER_IDS.GRANDPA.value, USERNAME, PASSWORD, SESSION)
-            if i == 2: scheduled_upload(USER_IDS.BENSON.value, USERNAME, PASSWORD, SESSION)
-            if i == 3: scheduled_upload(USER_IDS.LYING.value,USERNAME, PASSWORD, SESSION)
-            if i == 4: scheduled_upload(USER_IDS.BRO.value, USERNAME, PASSWORD, SESSION)
+            # if i == 1: scheduled_upload(USER_IDS.GRANDPA.value, USERNAME, PASSWORD, SESSION)
+            # if i == 2: scheduled_upload(USER_IDS.BENSON.value, USERNAME, PASSWORD, SESSION)
+            # if i == 3: scheduled_upload(USER_IDS.LYING.value,USERNAME, PASSWORD, SESSION)
+            # if i == 4: scheduled_upload(USER_IDS.BRO.value, USERNAME, PASSWORD, SESSION)
+            scheduled_upload(USER_IDS[i], USERNAME, PASSWORD, SESSION)
         except Exception as e:
-            print(f"Doesn't work for {USERNAME}")
+            print(f"Doesn't work for {USERNAME.replace('.','_').replace('_','.')}") # To print username to action logs
             print(e)
             continue
-        # TODO: ACCOUNT 3 USER, PASS, SESSIOn
         # elif i == 4, i == 5, ... for future accounts
---
+++ depreciated_id_user.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class USER_IDS(Enum):
+    IDS = [
+            67987126316,    # GRANDPA
+            67538479126,    # BENSON
+            69469114299,    # LYING
+            69475320768     # BRO
+         ]
+
+    #
+    # GRANDPA=67987126316
+    # BENSON=67538479126
+    # LYING=69469114299
+    # BRO=69475320768
+    #
